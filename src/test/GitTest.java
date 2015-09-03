a9e11363e8054fb7438549c9cd5e80bb11377cc1
obataka <obataka123@gmail.com>
2015-09-03 23:34:42 +0900

    sutashからのコンフリクト解消

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("First Stdout");
+		System.out.println("First Stdout2");
+		System.out.println("First Stdout3");
 	}
 
 }
